93d499821c5a190368d833f11c1ca14a796e7705
Silongo Vailolo <silongo68@hotmail.com>
2025-03-04 16:44:12 +1000

    Done events lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "dodgerblue"
+dodger.style.left = dodger.style.left || "0px"
+
+
+    function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "")
+        const left = parseInt(leftNumbers, 10)
+
+        if (left > 0) {
+            dodger.style.left = `${left - 20}px`
+        }    
+    };
+
+    function moveDodgerRight() {
+        const leftNumbers = dodger.style.left.replace("px", "")
+            const left = parseInt(leftNumbers, 10);
+
+            if (left < 360) {
+                dodger.style.left = `${left + 20}px`
+            }
+    }
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft") {
+            moveDodgerLeft();
+        } else if (event.key === "ArrowRight") {
+            moveDodgerRight();
+        }
+    })
+
